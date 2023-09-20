06dd35308fd625cdd207d535b7e213e5a3638f0b
ivoquaglia <ivoquaglia99@gmail.com>
2023-09-20 12:28:16 -0300

    juego react

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react"
+import confetti from "canvas-confetti"
+import { Square } from "./components/Square"
+import { TURNS, WINNER_COMBOS } from "./constantes"
+import { WinnerModal } from "./components/WinnerModal"
+
+
+
+export const App = () => {
+
+    const [board, setBoard] = useState(Array(9).fill(null))
+
+    const [turn, setTurn] = useState(TURNS.x)
+
+    const [winner, setWinner] = useState(null)
+
+    const checkWinner = (boardToCheck) => {
+        for(const combo of WINNER_COMBOS){
+            const [a, b, c] = combo
+            if(
+                boardToCheck[a] && 
+                boardToCheck[a] === boardToCheck[b] &&
+                boardToCheck[a] === boardToCheck[c]
+            ){
+                return boardToCheck[a]
+            }
+        }
+        return null
+    }
+
+    const checkEndGame = (newBoard) => {
+        return newBoard.every((Square) => Square !== null)
+    }
+
+    const resetGame = () => {
+        setBoard(Array(9).fill(null))
+        setTurn(TURNS.x)
+        setWinner(null)
+    }
+
+    const updateBoard = (index) => {
+
+        if(board[index] || winner) return
+        
+        const newBoard = [...board]
+        newBoard[index] = turn
+        setBoard(newBoard)
+
+        const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
+        setTurn(newTurn)
+
+        const newWinner = checkWinner(newBoard)
+        if (newWinner) {
+            confetti()
+            setWinner(newWinner)
+        }else if(checkEndGame(newBoard)){
+            setWinner(false)
+        }
+    }
+
+
+
+    return (
+        <main className="board">
+            <h1>Ta Te Ti</h1>
+            <button onClick={resetGame}>Reset del juego</button>
+            <section className="game">
+                {board.map((square, index) => {
+                    return (
+                        <Square
+                            key={index}
+                            index={index}
+                            updateBoard={updateBoard}
+                        >
+                        {square}
+                        </Square>
+                    )
+                })}
+            </section>
+            <section className="turn">
+                <Square
+                isSelected={turn === TURNS.x}
+                >{TURNS.x}</Square>
+                <Square
+                isSelected={turn === TURNS.o}
+                >{TURNS.o}</Square>
+            </section>
+
+            <WinnerModal winner={winner} resetGame={resetGame}/>
+
+        </main>
+    )
+}
